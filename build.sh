7ab4677f4c72463656cdde8e37bc3988ee728a73
gyara <me@gyara.moe>
2021-04-18 18:27:29 +0900

    build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+
+mkdir -p /tmp/mrca
+
+working_dir=$(pwd)
+for t in `fd --type=d`
+do
+    cp ${t} /tmp/mrca/ -r
+    cd /tmp/mrca/${t}
+    paru -U --localrepo --cachedir /tmp/paru  --cleanafter
+    cd ${working_dir}
+done
