513bb94303ba50dbc345fb2643e229e79107bf18
gyara <me@gyara.moe>
2021-06-01 21:20:00 +0900

    update sign

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ for t in `fd --type=d`
 do
     cp ${t} /tmp/mrca/ -r
     cd /tmp/mrca/${t}
-    paru -U --localrepo --cachedir /tmp/paru  --cleanafter
+    paru -U --localrepo --cachedir /tmp/paru  --cleanafter --sign --signdb
     cd ${working_dir}
 done
